46f443f1e567cbcde6abfcf218a2bb728c26ef6a
Gohar Vardanyan <goharvardanyan@Gohars-MacBook-Pro.local>
2024-08-06 01:22:22 -0700

    Removed unnesessary file

--- DigitainProject/import Combine  protocol NetworkManagerProtocol {     func downloadJSON(from urlString- String) -> AnyPublisher<Data, NetworkError>     func fetchAndDecode<T- Decodable>(from urlString- String, as type- T.Type) -> AnyPublisher<T, NetworkError> }.swift
+++
@@ -1,8 +0,0 @@
-//
-//  import Combine  protocol NetworkManagerProtocol {     func downloadJSON(from urlString- String) -> AnyPublisher<Data, NetworkError>     func fetchAndDecode<T- Decodable>(from urlString- String, as type- T.Type) -> AnyPublisher<T, NetworkError> }.swift
-//  DigitainProject
-//
-//  Created by Gohar Vardanyan on 8/2/24.
-//
-
-import Foundation
